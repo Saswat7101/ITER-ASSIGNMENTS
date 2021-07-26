b76b3f3e7750cfaa183c07a8e066c60d135a6825
Saswat Mohanty <mohantysaswat154@gmail.com>
2021-07-26 21:34:10 +0530

    added assignment 7

---
+++ Semester - 2/Assignment 7/src/Part1.java
@@ -0,0 +1,86 @@
+import java.util.*;
+public class Part1 {
+	public static final int MAX=5;
+	public static int front=-1;
+	public static int rear=-1;
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int queue[]=new int[MAX];
+		while(true)
+		{
+			System.out.println("***MENU***");
+			System.out.println("0: Exit");
+			System.out.println("1: Insert");
+			System.out.println("2: Delete");
+			System.out.println("3: Display");
+			System.out.println("Enter your choice: ");
+			int choice=sc.nextInt();
+			switch(choice)
+			{
+				case 0:
+					System.out.println("Exitting Program ");
+					System.exit(0);
+					break;
+				case 1:
+					insert(queue);
+					break;
+				case 2:
+					delete(queue);
+					break;
+				case 3:
+					display(queue);
+					break;
+				default:
+				System.out.println("Invalid choice");
+			}
+		}
+	}
+	public static void insert(int Q[])
+	{
+		if(isFull())
+		{
+			System.out.println("Error : Stack OverFlow");
+		}
+		else
+		{
+			Scanner in = new Scanner(System.in);
+			System.out.print("Enter value to be inserted : ");
+			Q[++rear]=in.nextInt();
+		}
+	}
+	public static void delete(int Q[])
+	{
+		if(isEmpty())
+		{
+			System.out.println("Error : Stack UnderFlow");
+		}
+		else
+		{
+			front++;
+		}
+	}
+	public static void display(int Q[])
+	{
+		if(isEmpty())
+		{
+			System.out.println("Error : Queue is Empty");
+		}
+		else
+		{
+			System.out.println("Queue Contains");
+		}
+			for(int i=front+1; i<=rear; i++)
+			{
+				System.out.println(Q[i]);
+			}
+	}
+	public static boolean isFull()
+	{
+		return rear==MAX-1;
+	}
+	public static boolean isEmpty()
+	{
+		return rear==front;
+	}
+}
---
+++ Semester - 2/Assignment 7/src/Part2.java
@@ -0,0 +1,100 @@
+import java.util.*;
+public class Part2 {
+	public static void main(String args[])
+	{
+		Scanner in = new Scanner(System.in);
+		Node front=null, rear=null;
+		
+		while(true)
+		{
+			System.out.println("****MENU****");
+			System.out.println("0 : Exit");
+			System.out.println("1 : Insert");
+			System.out.println("2 : Delete");
+			System.out.println("3 : Display");
+			System.out.println("Enter your Choice: ");
+			int sw=in.nextInt();
+			switch(sw)
+			{
+				case 0:
+					System.out.println("Exitting Program ");
+					System.exit(0);
+					break;
+				case 1:
+					front=insert(rear, front);
+					rear=getRear(front);
+					break;
+				case 2:
+					front=delete(rear, front);
+					break;
+				case 3:
+					display(rear, front);
+					break;
+				default:
+					System.out.println("Wrong Option Please Try Again");
+			}
+		}
+	}
+	public static Node insert(Node rear, Node front)
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the value to insert in Queue : ");
+		Node n = new Node(in.nextInt());
+		if(rear==null || front==null)
+		{
+			front = n;
+			rear = n;
+		}
+		else
+		{
+			rear.next=n;
+			rear=n;
+		}
+		return front;
+	}
+	public static Node delete(Node rear, Node front)
+	{
+		if(front==null)
+		{
+			System.out.println("Error : Stack UnderFlow");
+		}
+		else
+		{
+			front=front.next;
+		}
+		return front;
+	}
+	public static void display(Node rear, Node front)
+	{
+		Node n = front;
+		if(n==null)
+		{
+			System.out.println("Queue is Empty");
+		}
+		else
+		{
+			System.out.println("Elements of Queue are : ");
+			while(n!=null)
+			{
+				System.out.println(n.info);
+				n=n.next;
+			}
+		}
+	}
+	public static Node getRear(Node front)
+	{
+		if(front==null)
+		{
+			return null;
+		}
+		else
+		{
+			Node n = front;
+			while(n.next!=null)
+			{
+				n=n.next;
+			}
+			return n;
+		}
+	}
+}
